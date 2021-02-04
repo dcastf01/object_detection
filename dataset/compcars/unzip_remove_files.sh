c031277236915dcd1b612a18ca34e08f1ea2d463
David Castellano <david@beiota.com>
2021-02-04 08:46:01 +0100

    Update unzip_remove_files.sh

--- dataset/compcars/unzip_remove_files.sh
+++ dataset/compcars/unzip_remove_files.sh
@@ -1,7 +1,7 @@
 password="d89551fd190e38"
 echo "comprimiendo en un zip"
-zip -F data.zip --out combined.zip
+zip -qq -F data.zip --out combined.zip 
 rm data.z*
 echo "descomprimiendo archivos"
-unzip -P $password  combined.zip
+unzip -qq -P $password  combined.zip 
 rm combined.zip
