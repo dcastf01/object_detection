fb306b0f16da3024f9dc85c14f9b22e768cea2f0
David Castellano <dcastf01@gmail.com>
2021-03-18 13:47:34 +0100

    Update compcar_analisis.py

--- dataset/compcars/compcar_analisis.py
+++ dataset/compcars/compcar_analisis.py
@@ -51,6 +51,28 @@ def get_df_from_files(images_path="/content/data/image",label_path="/content/dat
 
         return row
     
+    def create_dataframe_from_image_path(images_path,level_image=3):
+        df=pd.DataFrame()
+
+
+        for root, dirs, files in tqdm(os.walk(images_path)):
+            level = root.replace(images_path, '').count(os.sep)
+
+
+            if level==level_image:
+                new_row=get_new_row_except_name_file(root)
+
+                for f in files:
+                    filename=f.split(".")[0]
+                    extension=f.split(".")[1]
+                    new_row["image_name"]=filename
+                    new_row["extension"]=extension
+                    
+                    df=df.append(new_row, ignore_index=True)
+                
+        
+        return df
+    
     df=create_dataframe_from_image_path(images_path)
     
     df=df.apply(lambda row: get_type_image(row),axis=1)
