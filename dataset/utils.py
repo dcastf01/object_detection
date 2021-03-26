508920cdb085ef17757b63675348e79af7d8a7ba
David Castellano <dcastf01@gmail.com>
2021-03-26 11:24:37 +0100

    Update utils.py

--- dataset/utils.py
+++ dataset/utils.py
@@ -2,6 +2,9 @@ from scipy.io import loadmat
 from dataset import config
 
 class CompcarsRealNames:
+    def __init__(self):
+        self.real_names_makes=self.load_real_names_compcars()["make_names"]
+        self.real_names_models=self.load_real_names_compcars()["model_names"]
         
     def load_real_names_compcars(self):
 
@@ -11,18 +14,19 @@ class CompcarsRealNames:
         return self.load_real_names_compcars()["make_names"]
 
     def get_real_make_name_with_index(self,index:int):
-        return self.real_make_names_compcars()[index]
+        return self.real_names_makes[index]
         
     def real_model_name_compcars(self):
         return self.load_real_names_compcars()["model_names"]
     
     def get_real_model_name_with_index(self,index:int):
-        return self.real_model_name_compcars()[index]
+        return self.real_names_models[index]
 
 def test():
     realnames=CompcarsRealNames()
     car_make_name=realnames.get_real_make_name_with_index(1)
+    
     print("car_make_name",car_make_name)
     car_model_name=realnames.get_real_model_name_with_index(1)
-    print("car_model_name",car_make_name)
-test()
+    print("car_model_name",car_model_name)
+# test()
