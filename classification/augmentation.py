472e7dd02fd355f1f9874e1844449929ad79205e
David Castellano <dcastf01@gmail.com>
2021-03-26 12:28:45 +0100

    creating files to new model on torch

--- classification/augmentation.py
+++ classification/augmentation.py
@@ -10,7 +10,6 @@ def get_transform_from_aladdinpersson():
             A.Resize(width=1920, height=1080),
             A.RandomCrop(width=1280, height=720,p=0.5),
             A.Rotate(limit=40, p=0.9, border_mode=cv2.BORDER_CONSTANT),
-            A.
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.1),
             A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.9),
