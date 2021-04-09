4a934612e0c78867117563763cba4d0ce8cff990
David Castellano <dcastf01@gmail.com>
2021-04-09 14:40:49 +0200

    delete spaces

--- classification/model/build_model.py
+++ classification/model/build_model.py
@@ -8,11 +8,11 @@ from classification.model.models_with_loss import ModelWithOneLoss
 def build_model(model_name:str,loss_fn=None,metrics:Union[None,list]=None):
     #el modelo debe de ser capaz de si le pasas las labels te calcule el resultado de la funcion de perdida
     
-    if model_name==CONFIG.architecture_name.torch_squeezenet:
+    if model_name==CONFIG.ARCHITECTURES_AVAILABLE.torch_squeezenet:
         #hace falta añadir la funcion de perdida
         net=get_squeezenet(CONFIG.NUM_CLASSES,loss_fn).to(CONFIG.DEVICE)
         model=ModelWithOneLoss(net)
-    elif model_name== CONFIG.architecture_name.torch_transFG:
+    elif model_name== CONFIG.ARCHITECTURES_AVAILABLE.torch_transFG:
         
         model=get_transFG(NUM_CLASS=CONFIG.NUM_CLASSES)
         
