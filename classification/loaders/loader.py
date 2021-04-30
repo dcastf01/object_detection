0e20e55c8cddc9d24b52a9c4b3ce900d7270e885
David Castellano <dcastf01@gmail.com>
2021-04-30 10:44:21 +0200

    new loader to detect cropped_images

--- classification/loaders/loader.py
+++ classification/loaders/loader.py
@@ -2,13 +2,19 @@
 import pandas as pd
 
 from torch.utils.data import Dataset
+import os
 class Loader (Dataset):
     
     def __init__ (self, df:pd.DataFrame,root_dir_images:str,
                   transform=None,condition_filter:str=None,
                   ):
         self.data=df
-        self.root_dir_images=root_dir_images
+        root_dir=root_dir_images.split(os.path.sep)[:-1]
+        root_dir_crooped_image=os.path.join(*root_dir,"cropped_image")
+        if os.path.exists(root_dir_crooped_image):
+            self.root_dir_images=root_dir_crooped_image
+        else:
+            self.root_dir_images=root_dir_images
         
         self.transform=transform
 
