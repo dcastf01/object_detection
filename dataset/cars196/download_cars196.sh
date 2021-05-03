5174143567f845ef91c32f61d7e5d8ec433caf16
[dcastf01] <[dcastf01@gmail.com]>
2021-05-03 13:57:25 +0200

    cars196 available

--- dataset/cars196/download_cars196.sh
+++ dataset/cars196/download_cars196.sh
@@ -1,7 +1,8 @@
 cd data/cars196
-wget http://imagenet.stanford.edu/internal/car196/cars_train.tgz
-wget http://imagenet.stanford.edu/internal/car196/cars_test.tgz
-wget http://imagenet.stanford.edu/internal/car196/cars_test_annos_withlabels.mat
+wget http://ai.stanford.edu/~jkrause/car196/cars_train.tgz
+
+wget http://ai.stanford.edu/~jkrause/car196/cars_test.tgz
+# wget http://imagenet.stanford.edu/internal/car196/cars_test_annos_withlabels.mat
 wget https://ai.stanford.edu/~jkrause/cars/car_devkit.tgz
 
 tar -xzvf cars_train.tgz 
