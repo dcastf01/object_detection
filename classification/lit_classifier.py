777dbcd48e3ed5f03b726e3dbe71925d94328878
[dcastf01] <[dcastf01@gmail.com]>
2021-05-03 08:54:26 +0200

    limpieza de codigo

--- classification/lit_classifier.py
+++ classification/lit_classifier.py
@@ -53,24 +53,20 @@ class LitClassifier(LitSystem):
 
         if isinstance(targets,list):
             targets=targets[0]
+        
         #lo ideal sería hacer algo tipo loss,preds=self.model(x) de esta manera al modelo se le 
         #incluiria el loss fn y así podremos hacer la tripleta
-        # loss=self.loss_fn(preds,targets)
         preds_probability=nn.functional.softmax(preds,dim=1)
         metric_value=self.train_metrics_base(preds_probability,targets)
         data_dict={"loss":loss,**metric_value}
 
         self.insert_each_metric_value_into_dict(data_dict,prefix="")
-        # self.log('train_loss',loss)
-        # self.log('train_metrics',metric_value)
-        
+
         
         return loss
     
     def validation_step(self, batch, batch_idx):
-        '''used for logging metrics'''
         x, targets = batch
-        
         preds=self.model(x)
         loss=self.criterion(preds,targets)
    
@@ -78,16 +74,7 @@ class LitClassifier(LitSystem):
             targets=targets[0]
         preds_probability=nn.functional.softmax(preds,dim=1)
         metric_value=self.valid_metrics_base(preds_probability,targets)
-        # metric_value={**metric_value,
-                    #   **self.valid_metric_auroc(preds.softmax(dim=0),targets)}
-        
         data_dict={"val_loss":loss,**metric_value}
-        #########CREAR UNA FUNCIÓN QUE COJA METRICA Y DATA DICT, Y SUELTE LA PREDICCION 
-        # ##################Y GENERE EL LOG CORRESPONDIENTE
-        # Log validation loss (will be automatically averaged over an epoch)
+  
         self.insert_each_metric_value_into_dict(data_dict,prefix="")
-        # self.log('val_loss', loss, on_step=False, on_epoch=True)
-        # self.log('val_metrics',metric_value, on_step=False, on_epoch=True)
-
-        # Log metrics
-        #self.log('valid_acc', self.accuracy(logits, y))
+        
