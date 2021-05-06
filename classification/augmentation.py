bddfcbfc3a688980be9691102e6af8a8716b5c08
[dcastf01] <[dcastf01@gmail.com]>
2021-05-06 10:52:55 +0200

    last version

--- classification/augmentation.py
+++ classification/augmentation.py
@@ -2,7 +2,7 @@ import albumentations as A
 import cv2
 from albumentations.pytorch import ToTensorV2
 from timm.data.constants import IMAGENET_DEFAULT_MEAN,IMAGENET_DEFAULT_STD
-
+from config import CONFIG
 # get from https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/Basics/albumentations_tutorial/full_pytorch_example.py
 def get_transform_from_aladdinpersson()->dict:
     train_transform = A.Compose(
@@ -17,7 +17,7 @@ def get_transform_from_aladdinpersson()->dict:
             #     A.Blur(blur_limit=3, p=0.5),
             #     A.ColorJitter(p=0.5),
             # ], p=1.0),
-            A.Resize(224,224),
+            A.Resize(CONFIG.IMG_SIZE,CONFIG.IMG_SIZE),
             A.Normalize(
                 # mean=[0, 0, 0],
                 mean=[0.485, 0.456, 0.406],
@@ -30,13 +30,13 @@ def get_transform_from_aladdinpersson()->dict:
     )
     val_transform = A.Compose(
         [
-            A.Resize(224,224),
+            A.Resize(CONFIG.IMG_SIZE,CONFIG.IMG_SIZE),
             # A.CenterCrop(height=128, width=128),
             A.Normalize(
-                # mean=[0, 0, 0],
-                mean=[0.485, 0.456, 0.406],
-                # std=[1, 1, 1],
-                std=[ 0.229, 0.224, 0.225],
+                mean=[0.5, 0.5, 0.5],
+                # mean=[0.485, 0.456, 0.406],
+                std=[1, 1, 1],
+                # std=[ 0.229, 0.224, 0.225],
                 max_pixel_value=255,
             ),
             ToTensorV2(),
